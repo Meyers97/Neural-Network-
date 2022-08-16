1e089de86030f9c854429eb72a0d5c299769ddf1
Josh Meyers <joshmeyers183@gmail.com>
2022-08-16 17:18:28 +1000

    update

---
+++ Activation_Function.java
@@ -0,0 +1,10 @@
+
+public class Activation_Function 
+{
+    //Sigmoid function 
+    public static double Sigmoid(double val)
+    {
+        return 1/(1+Math.exp(-val)); 
+    }
+
+}
--- Matrix_Arithmetic.java
+++ Matrix_Arithmetic.java
@@ -18,13 +18,13 @@ public class Matrix_Arithmetic
 
         double[][] weight = new double[length][n_neurons];
 
-        for(int ind_1 = 0; ind_1 < length; ind_1++)
+        for(int j = 0; j < length; j++)
         {
-            for(int ind_2 = 0; ind_2 < weight[ind_1].length; ind_2++)
+            for(int k = 0; k < weight[j].length; k++)
             {
-                weight[ind_1][ind_2] = (random.nextInt(200)-100)/100.0;
+                weight[j][k] = (random.nextInt(200)-100)/100.0;
                 if(bias == true)
-                    weight[length-1][ind_2] = 1;
+                    weight[length-1][k] = 1;
             }
         }
         return weight; 
@@ -36,9 +36,9 @@ public class Matrix_Arithmetic
         double[] weight = new double[n_neurons];
         Random random = new Random();
 
-        for(int ind_1 = 0; ind_1 < weight.length; ind_1++)
+        for(int j = 0; j < weight.length; j++)
         {
-            weight[ind_1] = (random.nextInt(200)-100)/100.0;
+            weight[j] = (random.nextInt(200)-100)/100.0;
         }
 
         return weight; 
@@ -46,6 +46,96 @@ public class Matrix_Arithmetic
 
     public static double[] Amplitude_Matrix_1D(double[] weighted_inputs)
     {
-        return null; 
+        double[] init_inputs = weighted_inputs;
+
+        for(int j = 0; j < init_inputs.length; j++)
+        {
+            // init_inputs[j] =  Activation_Function.Sigmoid(init_inputs[j]);
+        }
+
+        return init_inputs; 
+    }
+
+    public static double[][] Amplitude_Matrix_2D(double[][] weighted_inputs)
+    {
+        double[][] init_inputs = weighted_inputs;
+
+        for(int j = 0; j < init_inputs.length; j++)
+        {
+            for(int k = 0; k < init_inputs[j].length; k++)
+            {
+                // init_inputs[j][k] = Activatoin_Function.Sigmoid(init_inputs[j][k]);
+            }
+        }
+
+        return init_inputs;
+    }
+
+    public static double Matrix_Dot_1D(double[] input, double[] weight)
+    {
+        double result = 0; 
+
+        for(int j = 0; j < input.length; j++)
+        {
+            result += input[j]*weight[j];
+        }
+
+        return result;
+    }
+
+    public static double[] Matrix_Dot_2D(double[] input, double[][] weights)
+    {
+        double[] result = new double[weights[0].length];
+        
+        for(int j = 0; j < weights[0].length; j++)
+        {
+            for(int k = 0; k < input.length; k++)
+            {
+                result[j] += weights[k][j]*input[k];
+            }
+        }
+        return result;
+    }
+
+    public static double[][] Matrix_Multiplication(double[][] input, double[][] weights)
+    {
+        double[][] result = new double[input.length][weights[0].length];
+
+        for(int j = 0; j < input.length; j++)
+        {
+            for(int k = 0; k < weights[0].length; k++)
+            {
+                for(int l = 0; l < input[0].length; l++)
+                {
+                    result[j][k] += input[j][l]*weights[l][k];
+                }
+            }
+        }
+        return result;
+    }
+
+    public static double[] Matrix_Addition_1D(double[] input_1, double[] input_2)
+    {
+        double[] result = new double[input_1.length];
+
+        for(int j = 0; j < input_1.length; j++)
+        {
+            result[j] = input_1[j] + input_2[j];
+        }
+        return null;
+    }
+
+    public static double[][] Matrix_Addition_2D(double[][] input_1, double[][] input_2)
+    {
+        double[][] result = new double[input_1.length][input_1[0].length];
+        
+        for(int j = 0; j < input_1.length; j++)
+        {
+            for(int k = 0; k < input_1[0].length; k++)
+            {
+                result[j][k] = input_1[j][k] +input_2[j][k];
+            }
+        }
+        return result;
     }
 }
