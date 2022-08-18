3ec004323b7f0b68b13f8a727d70f0684938e150
Josh Meyers <joshmeyers183@gmail.com>
2022-08-18 16:01:15 +1000

    Leaky_RELU

--- Activation_Function.java
+++ Activation_Function.java
@@ -7,4 +7,19 @@ public class Activation_Function
         return 1/(1+Math.exp(-val)); 
     }
 
+    
+    public static double[] Leaky_RELU(double[] val)
+    {
+        double[] result = new double[val.length];
+
+        for(int j = 0; j < val.length; j++)
+        {
+            if(val[j] > 0 )
+                result[j] = val[j];
+            else
+                result[j] = val[j]*0.01;
+        }
+
+        return result; 
+    }
 }
